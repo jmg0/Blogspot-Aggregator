c26ceacbb6a6324c5afdb3c1d8f3c22e4db4b2cf
James Garay <jmgaray11@gmail.com>
2020-12-14 11:30:05 -0600

    extract_content()

--- blog.py
+++ blog.py
@@ -8,4 +8,36 @@ import nltk
 import textblob
 import os
 import altair as alt
+import hidden
 
+"""
+Parameters:
+    url - a url string representing valid API request
+
+Returns:
+    content - a list of strings representing HTML of blog
+            posts
+"""
+def extract_content(url=hidden.url):
+    resp = requests.get(url)
+    resp = resp.json()
+    content = list()
+    next_page_token = True
+    while next_page_token:
+        for post in resp.get('items', None):
+            content.append(post['content'].replace('\n', ''))
+        next_page_token = resp.get('nextPageToken', None)
+        if next_page_token:
+            resp = requests.get(f'{url}&pageToken={next_page_token}').json()
+    return content
+
+"""
+Parameters:
+    url - a url string representing valid API request
+
+Returns:
+    content - a list of strings representing text of blog posts
+"""
+def extract_content_text_only(url=hidden.url):
+    content = extract_content()
+    for i in range(len(content)):
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-
+import blog
 
 def main():
     print('hello')
+    print(blog.extract_content())
 
 if __name__ == '__main__':
     main()
