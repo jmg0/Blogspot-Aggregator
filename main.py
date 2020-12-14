54793dff5b76ad14a3d648c675b70e4974cd1052
James Garay <jmgaray11@gmail.com>
2020-12-14 16:46:19 -0600

    testing analyses

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def main():
     # print(blog.extract_content())
     # print(blog.extract_content_text_only())
     # print(blog.generate_markdown_files(8))
-    # blog.test_analysis()
+    blog.test_analysis()
     # print(blog.review_info())
-    blog.test_analysis2()
+    # blog.test_analysis2()
 
 
 if __name__ == '__main__':
